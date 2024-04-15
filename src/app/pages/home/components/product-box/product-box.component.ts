e9d039730a944e49bacfac4276d84e09b9caa057
IgorCRH <125159948+IgorCRH@users.noreply.github.com>
2024-04-15 20:22:53 -0300

    Add files via upload

---
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -0,0 +1,22 @@
+import { Component, EventEmitter, Input, Output} from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import { Produto } from 'app/models/produto.model';
+
+@Component({
+  selector: 'app-product-box',
+  standalone: true,
+  imports: [MatIconModule, CommonModule, MatCardModule],
+  templateUrl: './product-box.component.html',
+  styleUrl: './product-box.component.css'
+})
+export class ProductBoxComponent {
+  @Input() fullWidthMode = false;
+  @Input() product: Produto | undefined;
+  @Output() addToCart = new EventEmitter();
+
+  adicionaraoCarrinho(): void {
+    this.addToCart.emit(this.product);
+  }
+}
